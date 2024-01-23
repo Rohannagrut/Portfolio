70c9c10ef85bfafba0291cf09163d2aeeadb6d12
Rohannagrut <rgnagrut_b20@ee.vjti.ac.in>
2024-01-23 23:58:27 +0530

    2nd commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ricardo | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  title: "Rohan | Personal Portfolio",
+  description: "Rohan is a full-stack developer with 6 months of experience.",
 };
 
 export default function RootLayout({
